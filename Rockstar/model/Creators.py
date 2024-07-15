ccb5ca4512822c3fc150d036d59784aeea20e88f
VPavlichko <vladyslav.pavlichko@gmail.com>
2024-07-16 00:48:48 +0300

    Added more authors, refactoring

--- Rockstar/model/Creators.py
+++ Rockstar/model/Creators.py
@@ -1,10 +1,20 @@
-class CreatorsDict:
-    creatorsDict: dict
-
-    def __int__(self):
-        self.CreatorsDict = {
-            "__-XyJluGaH-__": 218888420,
-            "_nice4ka_": 120133800,
-            "XyJluGaH_banned2": 174812747,
-            "I_Kasper_I": 106510446
-        }
+def getCreatorsDict():
+    return {
+        "__-XyJluGaH-__": 218888420,
+        "_nice4ka_": 120133800,
+        "XyJluGaH_banned2": 174812747,
+        "I_Kasper_I": 106510446,
+        "kovalski84": 10157262,
+        "Gazastreifen": 135324404,
+        "matheuxzs": 187329435,
+        "PIKAMON_YT": 106555177,
+        "_X_y_Jl_u_G_a_H_": 208910807,
+        "_DragoN_BlacK_": 158840741,
+        "TheChenko": 64321874,
+        "BOMJARIK_": 182344635,
+        "Pulsar_ByeByeGTA": 153950711,
+        "XyJluGaH_banned": 70349051,
+        "SN3PR1X": 128886854,
+        "__-Kasper-__": 91981613,
+        "racesmurf123": 174938475
+    }
