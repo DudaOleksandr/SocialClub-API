3b2d94856209051e795f68c107a0f5aeb0cce02e
Oleksandr Duda <duda.oleks@gmail.com>
2024-07-15 23:48:08 +0300

    Updated main to default

--- Main.py
+++ Main.py
@@ -10,18 +10,7 @@ EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 SILENT = bool(int(os.environ['SILENT']))
 
-TARGET = "O_D_I_U_M"
-
-# # Main script to retrieve RID and avatar URL
-# client = RockstarClient(EMAIL, PASSWORD, SILENT)
-# client.Startup(force_renewing=True)
-# (rid, avatar_url), state = client.RetrieveRID(TARGET)
-#
-# # Results
-# if state == 1:
-#     print(f"RID of player {TARGET} is {rid}.\nAvatar URL : {avatar_url}")
-# else:
-#     print(f"An error occured while retriving the Rockstar ID of player {TARGET}.")
+TARGET = "TARGET"
 
 client = RockstarClient(EMAIL, PASSWORD, SILENT)
 client.startup(force_renewing=False)
