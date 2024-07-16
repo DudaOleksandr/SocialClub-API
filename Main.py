4d0be2a1b89bcff096cad5cde9f411ce9873d2da
Oleksandr Duda <duda.oleks@gmail.com>
2024-07-16 18:12:28 +0300

    Added func to get user from token

--- Main.py
+++ Main.py
@@ -14,18 +14,19 @@ key = os.environ['DB_KEY']
 EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 SILENT = bool(int(os.environ['SILENT']))
-TARGET = "O_D_I_U_M"
+TARGET = "TARGET"
 
 db = create_client(url, key)
 client = RockstarClient(EMAIL, PASSWORD, SILENT)
 client.startup(force_renewing=False)
 (rid, avatar_url), state = User.retrieve_rid(TARGET, client.get_token())
 jobs = Jobs.get_jobs_by_username(TARGET, client.get_token())
-user = User.retrieve_user_from_creds(client.get_token())
+user = User.retrieve_user_from_token(client.get_token())
 job_list = parseJobs(jobs)
 if state == 1:
     print(f"RID of player {TARGET} is {rid}.\nAvatar URL : {avatar_url}")
     print(f"Jobs of player {TARGET} are {job_list}.")
+    print(f"Current user is: {user}.")
 else:
     print(f"An error occured while retriving the Rockstar ID of player {TARGET}.")
 
