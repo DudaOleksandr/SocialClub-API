e465c203eaa86026842613e0a3bfb82556ebdf63
Oleksandr Duda <duda.oleks@gmail.com>
2024-07-16 20:26:55 +0300

    Merge issues fix

--- Main.py
+++ Main.py
@@ -7,19 +7,19 @@ from Rockstar.util.Parser import parseJobs
 from Rockstar.DAL.DbClient import DbClient
 
 load_dotenv()
-# Constants
 
 EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 SILENT = bool(int(os.environ['SILENT']))
-TARGET = "TARGET"
+TARGET = "Kurwocka"
 
-db = create_client(url, key)
 client = RockstarClient(EMAIL, PASSWORD, SILENT)
 client.startup(force_renewing=False)
 (rid, avatar_url), state = User.retrieve_rid(TARGET, client.get_token())
 jobs = Jobs.get_jobs_by_username(TARGET, client.get_token())
 job_list = parseJobs(jobs)
+user = User.retrieve_user_from_token(client.get_token())
+
 if state == 1:
     print(f"RID of player {TARGET} is {rid}.\nAvatar URL : {avatar_url}")
     print(f"Jobs of player {TARGET} are {job_list}.")
